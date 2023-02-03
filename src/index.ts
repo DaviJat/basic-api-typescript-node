44d01e1415b28e73ccbd88b6c53947269ee7a9c1
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2023-02-02 21:11:51 -0300

    Eslint + Prettier Configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from 'express'
+import express from 'express';
 
-const server = express()
+const server = express();
 
-server.get('/', (req, res) =>{
-    res.send('Hello World!');
-})
+server.get('/', (req, res) => {
+	res.send('Hello World!');
+});
 
 server.listen(3333, () => {
-    console.log('Server started on port 3333')
-})
+	console.log('Server started on port 3333');
+});
