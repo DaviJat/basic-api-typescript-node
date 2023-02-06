475d8a01e8fb264f04cc75528ddc431f192c419e
Davi Jatobá <91758001+DaviJat@users.noreply.github.com>
2023-02-06 20:22:34 -0300

    Creating basic crud Controller and Router

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
 import express from 'express';
 
+import { userRouter } from './modules/user/userRouter.js';
+
+// Start expresser server
 const server = express();
 
+// Server use json req
+server.use(express.json);
+
+// Routes the HTTP GET Requests to the path
 server.get('/', (req, res) => {
-	res.send('Hello World!');
+	res.send('Hello World');
 });
 
+server.use('/user', userRouter);
+
+// Listens for connections on a given path
 server.listen(3333, () => {
 	console.log('Server started on port 3333');
 });
